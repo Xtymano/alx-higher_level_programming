2960733ec4c0848d4cfe878fbf49c5e4dee53964
Xtymano <xtymano@gmail.com>
2024-01-19 03:51:35 -0800

    0-main.py

--- 0x06-python-classes/0-main.py
+++ 0x06-python-classes/0-main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 Square = __import__('0-square').Square
 """This class represents a square with a side length"""
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
+my_square = Square() print (mysquare))
+print(mysquare.dict_)
